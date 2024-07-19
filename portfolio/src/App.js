832fa798436f579c5238eb22be283ad7c4a4acad
Inshal Farid <inshal.farid@hdt-ca.com>
2024-07-19 13:39:00 -0400

    added the react project

---
+++ portfolio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+// basic react app
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Portfolio</h1>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
