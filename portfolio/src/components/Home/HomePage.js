7986b046d82fc238afc0a00eb817df9a67d30761
Inshal Farid <inshal.farid@hdt-ca.com>
2024-07-26 13:58:03 -0400

    The profile, job status, projects and fun facts section is done

--- portfolio/src/components/Home/HomePage.js
+++ portfolio/src/components/Home/HomePage.js
@@ -2,27 +2,52 @@ import React from 'react';
 import './HomePage.css';
 import images from '../../images/index'; // Adjust the path as needed
 
-
 const HomePage = () => {
   return (
     <div className="home-page">
       <div className="first-column">
-        <div className="block profile">
-            {/* Profile icon */}
+        <div className="block-profile">
+          <div className="profile-1st-container">
             <div className="profile-icon">
               <img src={images.pic} alt="Profile Icon" />
             </div>
-            <div className="profile-name">Muhammad Inshal Farid</div>
+            <div className="profile-name">
+              <h4>Muhammad Inshal Farid</h4>
+              <h6>@Software Developer</h6>
+              
+            </div>
+            
+            <div className="profile-linkedin-icon">
+              <img src={images.linkedinIcon} alt="Indeed Icon" />
+            </div>
          </div>
-        <div className="block job-status">
-          Job Status?
+         <div className="profile-2nd-container">
+            <div className="Github-Icon">
+              <img src={images.githubIcon} alt="Github Icon" />
+            </div>
+            <div className="resume-button">
+              <button>Download Resume</button>
+            </div>
+         </div>
+       </div>
+        <div className="block-job-status">
+          <div className="job-status">
+            <h6>Currently</h6>
+            <h4>Software Developer</h4>
+            <h7>@Bend All Automotives</h7>
+          </div>
+          
         </div>
         <div className='projects-fun-container'>
-          <div className="block projects">
-            Projects
+          <div className="block-projects-fun">
+            <div className="projects-funfacts-icon">
+              <img src={images.ProjectsIcon} alt="Projects Icon" />
+            </div>
           </div>
-          <div className="block fun-facts">
-            Fun Facts About Me
+          <div className="block-projects-fun">
+            <div className="projects-funfacts-icon">
+              <img src={images.funFactsIcon} alt="Fun Facts Icon" />
+            </div>
           </div>
         </div>
         <div className="block achievements">
--- portfolio/src/images/index.js
+++ portfolio/src/images/index.js
@@ -1,7 +1,15 @@
 import pic from './pic.jpg';
+import linkedinIcon from './linkedinIcon.png';
+import githubIcon from './githubIcon.png';
+import ProjectsIcon from './ProjectsIcon.png';
+import funFactsIcon from './funFactsIcon.png';
 
 const images = {
   pic,
+  linkedinIcon,
+  githubIcon,
+  ProjectsIcon,
+  funFactsIcon,
 };
 
 export default images;
