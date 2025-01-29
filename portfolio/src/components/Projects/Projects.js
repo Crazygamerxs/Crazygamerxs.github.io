5838d0451b0ac6a2b7d7d7c93c8f8184643b8772
Crazygamerxs <m.inshalfarid@gmail.com>
2025-01-29 11:42:35 -0500

    Updated the projects page

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -1,16 +1,39 @@
 import React, { useState } from 'react';
 import TopBar from '../Common/topBar';
-import images from '../../images/index'; // Adjust the import path as needed
-import './Projects.css'; 
+import images from '../../images/index';
+import './Projects.css';
 
 const imageArray = [
-  { src: images.civfix, title: "Civfix", skills: ["Python", "Django", "JS", "Html/Bootstrap", "SQLite"] },
-  { src: images.Camsentinel, title: "CamSentinel", skills: ["Python", "Flask", "Sqlite", "Opencv", "Raspberrypi"] },
-  { src: images.tuxania, title: "Tuxania", skills: ["Ignition", "Html/Css", "JS", "Python", "Sql"] },
-  { src: images.dreamspace, title: "Dreamspace", skills: ["Python", "Flask", "Three.js", "ArCore", ""] },
-  { src: images.Penpal, title: "PenPal", skills: ["Node.js", "Html/Css", "C++", "Sql"] }
-
-  // Add more images and skills as needed
+  { 
+    src: images.civfix, 
+    title: "Civfix", 
+    skills: ["Python", "Django", "JS", "Html/Bootstrap", "SQLite"],
+    link: "https://github.com/Crazygamerxs/CivFix"
+  },
+  { 
+    src: images.Camsentinel, 
+    title: "CamSentinel", 
+    skills: ["Python", "Flask", "Sqlite", "Opencv", "Raspberrypi"],
+    link: "https://github.com/jerinpious/CSCN72030-Sec2-Group1"
+  },
+  { 
+    src: images.tuxania, 
+    title: "Tuxania", 
+    skills: ["Ignition", "Html/Css", "JS", "Python", "Sql"],
+    link: "https://github.com/Crazygamerxs/Tuxania"
+  },
+  { 
+    src: images.dreamspace, 
+    title: "Dreamspace", 
+    skills: ["Python", "Flask", "Three.js", "ArCore", ""],
+    link: "https://github.com/Crazygamerxs/DreamSpace-Interior-Design"
+  },
+  { 
+    src: images.Penpal, 
+    title: "PenPal", 
+    skills: ["Node.js", "Html/Css", "C++", "Sql"],
+    link: "https://github.com/Crazygamerxs/Community-Marketplace-for-College-Students"
+  }
 ];
 
 function Projects() {
@@ -24,6 +47,10 @@ function Projects() {
     setCurrentImageIndex((prevIndex) => 
       (prevIndex - 1 + imageArray.length) % imageArray.length
     );
+  };
+
+  const handleImageClick = () => {
+    window.open(imageArray[currentImageIndex].link, '_blank');
   };
 
   const progressWidth = (currentImageIndex + 1) / imageArray.length * 100;
@@ -45,17 +72,17 @@ function Projects() {
               ))}
             </div>
           </div>
-          <div className="main-image-container">
+          <div className="main-image-container" onClick={handleImageClick} style={{ cursor: 'pointer' }}>
             <img
               src={imageArray[currentImageIndex].src}
               alt="Current"
               className="main-image"
             />
-            {/* <img
+            <img
               src={images.open_icon}
               alt="Open Icon"
               className="open-icon"
-            /> */}
+            />
             <div className="skills-container">
               {imageArray[currentImageIndex].skills.map((skill, index) => (
                 <div key={index} className="skill-box">{skill}</div>
@@ -76,27 +103,17 @@ function Projects() {
           </div>
         </div>
         <div className="button-container">
-          <button 
-            className="navigation-button previous-button" 
-            onClick={previousImage}
-          >
-            <img src={images.leftarrow} alt="Previous" />
-            <span>Previous</span>
+          <button className="previous-button" onClick={previousImage}>
+            <img src={images.leftarrow} alt="Left Arrow" />
+            <span style={{ marginTop: '6px' }}>Back</span>
           </button>
-          
-          <div className="progress-container">
-            <div 
-              className="progress-line" 
-              style={{ width: `${progressWidth}%` }}
-            />
+          <div className="line-container">
+            <div className="line"></div>
+            <div className="progress-line" style={{ width: `${progressWidth}%` }}></div>
           </div>
-          
-          <button 
-            className="navigation-button next-button" 
-            onClick={nextImage}
-          >
-            <span>Next</span>
-            <img src={images.rightarrow} alt="Next" />
+          <button className="next-button" onClick={nextImage}>
+            <span style={{ marginTop: '6px', marginLeft: '2px'}}>Next</span>
+            <img src={images.rightarrow} alt="Right Arrow" />
           </button>
         </div>
       </div>
